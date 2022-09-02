aa2a2eca528527b7c580e3596d7db40be8d84a44
Vadymbo-here <vadymbo@i.ua>
2022-09-02 21:50:17 +0300

    Full refactoring FS
    Little bit more progress on 2nd LAB

---
+++ More_About_Classes/More_about_classes/src/Main.java
@@ -0,0 +1,11 @@
+import myFirPack.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        House b = new House(123, 2, 4, 64, 23, "Booo");
+
+        System.out.println(b.toString());
+    }
+}
---
+++ More_About_Classes/More_about_classes/src/myFirPack/House.java
@@ -0,0 +1,83 @@
+package myFirPack;
+
+public class House {
+
+    private int id;
+    private int flatNumber;
+    private double square;
+    private int floor;
+    private int rooms;
+    private String street;
+
+    public House() {
+    }
+
+    public House(int id, int flatNumber, double square, int floor, int rooms, String street) {
+        this.id = id;
+        this.flatNumber = flatNumber;
+        this.square = square;
+        this.floor = floor;
+        this.rooms = rooms;
+        this.street = street;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFlatNumber() {
+        return this.flatNumber;
+    }
+
+    public void setFlatNumber(int flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public double getSquare() {
+        return this.square;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public int getFloor() {
+        return this.floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getRooms() {
+        return this.rooms;
+    }
+
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", flatNumber='" + getFlatNumber() + "'" +
+                ", square='" + getSquare() + "'" +
+                ", floor='" + getFloor() + "'" +
+                ", rooms='" + getRooms() + "'" +
+                ", street='" + getStreet() + "'" +
+                "}";
+    }
+}
